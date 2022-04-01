2d374ceb8430049ce108eed97b88df6dc990b9c2
Lfu001 <goodspeed913@gmail.com>
2022-04-01 16:04:05 +0900

    Add attributes-of-a-tensor.py

---
+++ introduction-to-pytorch/tensors/attributes-of-a-tensor.py
@@ -0,0 +1,12 @@
+#
+# Attributes of a Tensor
+#
+
+import torch
+
+if __name__ == "__main__":
+    tensor = torch.rand(3, 4)
+
+    print(f"Shape of tensor: {tensor.shape}")
+    print(f"Datatype of tensor: {tensor.dtype}")
+    print(f"Device tensor is stored on: {tensor.device}")
