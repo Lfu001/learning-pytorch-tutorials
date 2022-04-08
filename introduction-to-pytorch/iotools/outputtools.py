16d45141e2dabfa34bb46a21c35ae5dad771d712
Lfu001 <goodspeed913@gmail.com>
2022-04-08 16:23:31 +0900

    Add parameter "border" to outputtools.print_divider

--- introduction-to-pytorch/iotools/outputtools.py
+++ introduction-to-pytorch/iotools/outputtools.py
@@ -1,14 +1,18 @@
-def print_divider(label=None, length=40):
-    divider = "=="
+def print_divider(label=None, border="=", length=40):
+    if not isinstance(border, str):
+        raise TypeError("Parameter border expects str, got {}.".format(type(border)))
+    elif len(border) != 1:
+        raise ValueError("Parameter border expects str of length 1, got {}.".format(len(border)))
+    divider = border * 2
     if label:
         if not isinstance(label, str):
-            raise ValueError("Parameter label expects str, got {}.".format(type(label)))
+            raise TypeError("Parameter label expects str, got {}.".format(type(label)))
         divider += " " + label + " "
     if not isinstance(length, int) and length > 0:
         raise ValueError("Parameter length expects int (>0), got {}.".format(length))
     else:
         if len(divider) < length:
-            divider += "=" * (length - len(divider))
+            divider += border * (length - len(divider))
     print()
     print(divider)
     print()
